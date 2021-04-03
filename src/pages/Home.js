c051a5bf0f41946b02738c9d84dbce26950f92be
vwiklund <v.wiklund@hotmail.com>
2021-04-03 13:11:37 +0200

    cleaning up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import LinkButton from "../components/LinkButton/LinkButton";
 import "./styling.css";
-
+//Renders homepage
 export const Home = () => (
   <div className="center">
     <LinkButton to="/searchbycity" ph="Search by city" />
